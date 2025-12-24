edb60eb11768431d2504e550ea0ac6dd02ac7b4d
ClintonOgembo <clintonogembo70@gmail.com>
2025-12-24 12:36:01 +0300

    simon game

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,11 +1,52 @@
+var userClickedPattern = [];
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"]
 
+var level = 0;
+if (!$(document).keydown(nextSequence)) {
+    $("h1").text("Level 0");
+}
+
+
+$(".btn").click(function () {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    // console.log(userClickedPattern);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour)
+
+});
+
 function nextSequence() {
+
+    $("h1").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+    console.log(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+
+    level++;
+
 }
 
-$("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+function playSound(colour) {
+    var audio = new Audio("sounds/" + colour + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+
+
+
 
